30629929335fc38d7154164950a79191af24d412
Phuc <cyhapun242@gmail.com>
2025-07-18 17:22:54 +0700

    Add new producs for client side

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,6 +3,7 @@ const Products = require('../../models/product.model');
 
 // [GET] /
 module.exports.index = async (req, res) => {
+    // Get featured products
     let featuredProducts = await Products.find({
         deleted: false,
         status: 'active',
@@ -10,9 +11,21 @@ module.exports.index = async (req, res) => {
     }).limit(10); // Limit the amount of featured product
 
     featuredProducts = productsHelper.calculateNewPrice(featuredProducts);
+    // End of get featured products
+
+    // Get new products
+    let newProducts = await Products.find({
+        deleted: false,
+        status: 'active',
+    }).sort({position: "desc"}).limit(6); // Limit the amount of new products
+
+    newProducts = productsHelper.calculateNewPrice(newProducts);
+    // End of get new products
+    
     
     res.render('client/pages/home/index.pug', {
         pageTitle: "Home page",
         featuredProducts: featuredProducts,
+        newProducts: newProducts,
     })
 };
